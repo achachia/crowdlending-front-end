8a8a7ff7451302eab75a47b6762676a71782a172
achachia <achachia2003@yahoo.fr>
2020-06-17 08:21:13 +0200

    V 1.0.1
    1. modification de la structure des templates
    2. ajout le composant accueil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { DashboardAdministratorComponent } from './dashboard-administrator/dashboard-administrator.component';
+import { IdentificationComponent } from './identification/identification.component';
+import { AccueilComponent } from './accueil/accueil.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 
 
- 
+
 const routes: Routes = [
-  { path: 'DashboardAdministrator', component: DashboardAdministratorComponent} 
+  { path: '', component: AccueilComponent},
+  { path: 'DashboardAdministrator', component: DashboardAdministratorComponent},
+  { path: 'Identification', component: IdentificationComponent}
 ];
  
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DashboardAdministratorComponent } from './dashboard-administrator/dashboard-administrator.component';
+import { IdentificationComponent } from './identification/identification.component';
 import { RoutingModule } from './app-routing.module';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardAdministratorComponent
+    DashboardAdministratorComponent,
+    IdentificationComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
