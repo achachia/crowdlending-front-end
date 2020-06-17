62e4f6bd0f28f6e3330a3e42cdf16d288ede223f
achachia <achachia2003@yahoo.fr>
2020-06-17 17:05:15 +0200

    V .0.0.5
    Update-fonction-one-signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
   title = 'crowdlending-front-end';
-
-  
-  ngOnInit() {
-    // tslint:disable-next-line:no-string-literal
-    const OneSignal = window['OneSignal'] || [];
-    console.log('Init OneSignal');
-    OneSignal.push(['init', {
-      appId: '7fa46d8b-d9a1-4c84-b5f5-1747ccfc5168',
-      autoRegister: false,
-      allowLocalhostAsSecureOrigin: true,
-      notifyButton: {
-        enable: false
-      }
-    }]);
-    console.log('OneSignal Initialized');
-    // tslint:disable-next-line:only-arrow-functions
-    OneSignal.push(function() {
-      console.log('Register For Push');
-      OneSignal.push(['registerForPushNotifications']);
-    });
-    // tslint:disable-next-line:only-arrow-functions
-    OneSignal.push(function() {
-      // Occurs when the user's subscription changes to a new value.
-      // tslint:disable-next-line:only-arrow-functions
-      OneSignal.on('subscriptionChange', function(isSubscribed) {
-        console.log('The user\'s subscription state is now:', isSubscribed);
-        // tslint:disable-next-line:only-arrow-functions
-        OneSignal.getUserId().then(function(userId) {
-          console.log('User ID is', userId);
-        });
-      });
-    });
-    }
 }
